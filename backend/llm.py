f9b15f2d9ae41496eb7e4e738da4d252080f6783
murahari <muramurahari@gmail.com>
2026-02-11 17:44:47 +0530

    responses will be in json format

--- backend/llm.py
+++ backend/llm.py
@@ -1,6 +1,23 @@
 import ollama
+from pydantic import BaseModel
+import platform
+from typing import List,Literal
+
+def get_os():
+    system = platform.system().lower()
+    print(system)
+system_rule = 'you are a linux command generator'
+
+class CommandResponse(BaseModel):
+    command: str
+    info: str
+    task: str
+    risk_level: Literal["Low","Medium","High"]
+
+
 
 messages = []
+messages.append({'role':'system','content':system_rule})
 while True:
     user_input = input("you: ")
     if user_input.lower() == "quit":
@@ -9,6 +26,7 @@ while True:
     response = ollama.chat(
         model='phi3',
         messages=messages,
+        format=CommandResponse.model_json_schema(),
         stream=True
     )
     response_content = ""
